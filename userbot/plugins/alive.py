fece1804ae15359ae626cff2f5e898625af39132
vishal-coder-hub <64121981+vishal-coder-hub@users.noreply.github.com>
2020-04-22 18:09:23 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -17,4 +17,4 @@ async def amireallyalive(alive):
                      f"`My owner`: {DEFAULTUSER}\n"
                      "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @Anonymous860\n"
                      "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
-                     ")
+                     )
